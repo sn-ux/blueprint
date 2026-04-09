c8f5397761d68803f298dccbea2481cfa45019bd
sn-ux <suryanathan@icloud.com>
2026-04-09 02:35:56 -0700

    fine tune hero copy position: move slightly up and left

--- app/page.tsx
+++ app/page.tsx
@@ -979,7 +979,7 @@ export default function LandingPage() {
           <div
             className="absolute z-10 flex flex-col pointer-events-none"
             style={{
-              top: "18%",
+              top: "13%",
               left: "6%",
               opacity: textVisible ? 1 : 0,
               transition: "opacity 0.4s ease-in-out",
